c4a0e519cbd8f513c30cafe61340cce8ec3608fe
artem <masi.masi.2006@mail.ru>
2025-09-11 11:32:34 +0000

    lab1 - lab2 start process

---
+++ app/resources/views/main/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <p>Lorem</p>
+@endsection
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [MainController::class, 'index']);
+Route::get('/full_image/{img}', [MainController::class, 'show']);
+
+Route::get('/about', function(){
+    return view('main.about');
+});
+
+Route::get('/contact', function(){
+    $array = [
+        'name'=>'Moscow Polytech',
+        'adres' => 'B. Semenovskaya h.38',
+        'email' => '..@maspolytech.ru',
+        'phone' => '8(499)232-2222'
+    ];
+    return view('main.contact', ['contact'=>$array]);
 });
