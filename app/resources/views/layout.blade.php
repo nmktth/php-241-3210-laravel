799bd19cb76da9ec29ea5be68a91089fd323866e
artem <masi.masi.2006@mail.ru>
2025-10-16 07:07:05 +0000

    dropdown fix

--- app/resources/views/layout.blade.php
+++ app/resources/views/layout.blade.php
@@ -91,6 +91,7 @@
         <footer class="bg-dark text-white text-center py-3 fixed-bottom">
           <p>Ильин Артём 241-3210</p>
         </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 
 </html>
